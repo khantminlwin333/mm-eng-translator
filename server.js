d6c88578f7a7b6c8920d2bbec03029440631cf20
khantminlwin <webdeveloperppro@gmail.com>
2025-03-23 21:13:35 +0630

    Update server configuration for Fly.io

--- server.js
+++ server.js
@@ -105,6 +105,6 @@ app.get('/model/check-version', async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 }); 
